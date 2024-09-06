5077a0ccae89e81c846e2b3ea3885790d1c98b4e
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-06 17:21:17 -0300

    ✨ feat/BSK-2: Establish database connection

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+	constructor() {
+		super({ log: ['warn', 'error'] });
+	}
+	onModuleInit() {
+		return this.$connect();
+	}
+	onModuleDestroy() {
+		return this.$disconnect();
+	}
+}
+// @Injectable()
+// export class PrismaService {
+// 	public client: PrismaClient;
+// 	constructor() {
+// 		this.client = new PrismaClient()
+// 	}
+// }
