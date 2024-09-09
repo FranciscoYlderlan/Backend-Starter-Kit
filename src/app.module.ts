caf83a82200969f3f96842ff7746bcf6c9cf68d1
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-09 11:23:03 -0300

    ✨ feat/BSK-2: Add environment variables capture using Config Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './controllers/create-user.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
-
+import { envSchema } from './env';
 @Module({
+	imports: [ConfigModule.forRoot({ validate: (env) => envSchema.parse(env), isGlobal: true })],
 	controllers: [AppController],
 	providers: [AppService, PrismaService],
 })
