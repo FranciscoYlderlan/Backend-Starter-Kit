823e071a102ed734b8c4a37b75dcf4500bfdcd0f
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-10 18:27:35 -0300

    �️ :card_file_box: hore/BSK-2: Create folder structure for domain layerr

---
+++ src/core/use-cases/base-use-case.ts
@@ -0,0 +1,6 @@
+interface UseCaseRequest {}
+interface UseCaseResponse {}
+
+export abstract class BaseUseCase<Request extends UseCaseRequest, Response extends UseCaseResponse> {
+  public abstract execute(params: Request): Promise<Response>;
+}
