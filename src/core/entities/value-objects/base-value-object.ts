bab63355eb18066bd33747da4ad6d293c66d959e
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-13 10:36:03 -0300

    :recycle: refactor/BSK-5: Change {} to empty interface {[k: string]: never} for stricter typing

--- src/core/entities/value-objects/base-value-object.ts
+++ src/core/entities/value-objects/base-value-object.ts
@@ -1,9 +1,14 @@
-export interface ValueObjectParams {}
+export interface ValueObjectParams {
+  [key: string]: never;
+}
 
 export abstract class BaseValueObject<Params extends ValueObjectParams> {
   protected constructor(params: Params) {}
 
-  public static transform<Input>(params: Input): BaseValueObject<Input> {
+  // Corrigido: Ajustando a assinatura do método estático transform
+  public static transform<Params extends ValueObjectParams>(
+    params: Params,
+  ): BaseValueObject<Params> {
     throw new Error("Method 'transform' must be implemented in subclasses.");
   }
 }
