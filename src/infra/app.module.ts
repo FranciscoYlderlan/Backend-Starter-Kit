0bdd53703ae8a28dda75a800c6a9b9ee8ac74025
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-13 11:55:35 -0300

    ✨ feat/BSK-5: Add authentication module into app module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
+
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { HttpModule } from './http/http.module';
+
 import { CreateUserController } from './http/controllers/create-user.controller';
 
 import { envSchema } from './env';
-import { HttpModule } from './http/http.module';
-import { DatabaseModule } from './database/database.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+
     HttpModule,
+    AuthModule,
     DatabaseModule,
   ],
   controllers: [CreateUserController],
