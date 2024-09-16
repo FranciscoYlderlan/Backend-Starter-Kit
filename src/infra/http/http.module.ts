530e3b41532bf89385b0e24f4ad901cac9796f32
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-16 09:59:30 -0300

    :package: chore/BSK-5: Install bcrypt library

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { AuthenticateController } from './controllers/authenticate.controller';
 import { CreateUserController } from './controllers/create-user.controller';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateUserController],
+  controllers: [CreateUserController, AuthenticateController],
 })
 export class HttpModule {}
