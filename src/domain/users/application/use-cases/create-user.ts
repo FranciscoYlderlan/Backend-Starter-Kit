a8faa09799de93d543e37aee535ae7edec0b3457
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-17 12:01:32 -0300

    ✨ feat/BSK-5: Create user repository

--- src/domain/users/application/use-cases/create-user.ts
+++ src/domain/users/application/use-cases/create-user.ts
@@ -1,5 +1,6 @@
 import { BaseUseCase } from 'src/core/use-cases/base-use-case';
 import { User, UserProps } from '../../enterprise/entities/user';
+import { UserRepository } from '../repositories/user-repository';
 
 export interface CreateUserInput extends UserProps {}
 
@@ -10,11 +11,15 @@ export class CreateUserUseCase extends BaseUseCase<
   CreateUserInput,
   CreateUserOutput
 > {
+  constructor(private userRepository: UserRepository) {
+    super();
+  }
+
   public async execute(params: CreateUserInput): Promise<CreateUserOutput> {
     const userProps = params;
 
     const user = new User(userProps);
-
+    await this.userRepository.create({ data: user });
     return { user };
   }
 }
