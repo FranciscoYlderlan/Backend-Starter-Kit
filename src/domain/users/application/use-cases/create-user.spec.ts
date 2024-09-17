6c9a67ab92267babdb473b626ab153f2336e86e2
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-17 12:02:21 -0300

    :white_check_mark: test/BSK-5: Update user creation test

--- src/domain/users/application/use-cases/create-user.spec.ts
+++ src/domain/users/application/use-cases/create-user.spec.ts
@@ -1,8 +1,42 @@
+import {
+  CreateRequest,
+  CreateResponse,
+  DeleteRequest,
+  DeleteResponse,
+  IndexRequest,
+  IndexResponse,
+  ShowRequest,
+  ShowResponse,
+  UpdateRequest,
+  UpdateResponse,
+} from 'src/core/repositories/base-repository';
 import { expect, test } from 'vitest';
+import { User } from '../../enterprise/entities/user';
+import { UserRepository } from '../repositories/user-repository';
 import { CreateUserUseCase } from './create-user';
 
+const fakeUsersRepository: UserRepository = {
+  create: async (
+    params: CreateRequest<Partial<User>>,
+  ): Promise<CreateResponse> => {
+    return { id: '' };
+  },
+  index: (params: IndexRequest): Promise<IndexResponse<User>> => {
+    throw new Error('Function not implemented.');
+  },
+  show: (params: ShowRequest): Promise<ShowResponse<User>> => {
+    throw new Error('Function not implemented.');
+  },
+  update: (params: UpdateRequest<Partial<User>>): Promise<UpdateResponse> => {
+    throw new Error('Function not implemented.');
+  },
+  delete: (params: DeleteRequest): Promise<DeleteResponse> => {
+    throw new Error('Function not implemented.');
+  },
+};
+
 test('Create an user', async () => {
-  const createUser = new CreateUserUseCase();
+  const createUser = new CreateUserUseCase(fakeUsersRepository);
 
   const { user: newUser } = await createUser.execute({
     name: 'User Test',
