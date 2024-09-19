3efc13c2602e79bd33a74a5d8a1d650dd9e7e56c
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-19 17:55:57 -0300

    ✅ test/BSK-5: Create test for slug

---
+++ src/domain/users/enterprise/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest';
+import { Slug } from './slug';
+
+test('it should be able to create a new slug from text', () => {
+  const { value: props } = Slug.transform({ value: 'Example value text' });
+
+  expect(props.value).toEqual('example-value-text');
+});
