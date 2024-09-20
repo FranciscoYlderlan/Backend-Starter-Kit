d8f7e0d2a810568d81292bd0fefb024782a4ac48
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-20 10:44:05 -0300

    ✨ feat/BSK-5: Add createdAt and updatedAt with automatic creation

--- src/domain/users/enterprise/entities/user.ts
+++ src/domain/users/enterprise/entities/user.ts
@@ -1,12 +1,21 @@
 import { BaseEntity } from 'src/core/entities/base-entity';
+import { Optional } from 'src/core/types/optional';
+import { UniqueID } from './value-objects/unique-id';
 export interface UserProps {
   name: string;
   email: string;
   avatar?: string;
   password: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class User extends BaseEntity<UserProps> {
+  static create(props: Optional<UserProps, 'createdAt'>, id?: UniqueID) {
+    const user = new User({ ...props, createdAt: new Date() }, id);
+    return user;
+  }
+
   public getName(): string {
     return this.props.name;
   }
@@ -21,5 +30,11 @@ export class User extends BaseEntity<UserProps> {
 
   public getAvatar(): string | undefined {
     return this.props.avatar;
+  }
+  public getCreatedAt(): Date {
+    return this.props.createdAt;
+  }
+  public getUpdatedAt(): Date | undefined {
+    return this.props.updatedAt;
   }
 }
