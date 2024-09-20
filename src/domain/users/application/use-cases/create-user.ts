8424eb9caa107acd037d47cd830462c3b6ac2208
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-20 11:06:14 -0300

    ✨ feat/BSK-5: Add slug as a property of user entity

--- src/domain/users/application/use-cases/create-user.ts
+++ src/domain/users/application/use-cases/create-user.ts
@@ -3,7 +3,8 @@ import { BaseUseCase } from 'src/core/use-cases/base-use-case';
 import { User, UserProps } from '../../enterprise/entities/user';
 import { UserRepository } from '../repositories/user-repository';
 
-export interface CreateUserInput extends Optional<UserProps, 'createdAt'> {}
+export interface CreateUserInput
+  extends Optional<UserProps, 'createdAt' | 'slug'> {}
 
 export interface CreateUserOutput {
   user: User;
