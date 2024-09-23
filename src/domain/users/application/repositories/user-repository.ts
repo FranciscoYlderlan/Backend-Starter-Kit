5b71a3e14721850d94d9a516ce4e47e6fbce4bc3
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-23 08:30:36 -0300

    ✨ feat/BSK-5: Create in-memory user repository

--- src/domain/users/application/repositories/user-repository.ts
+++ src/domain/users/application/repositories/user-repository.ts
@@ -2,13 +2,20 @@ import {
   BaseRepository,
   CreateRequest,
   CreateResponse,
+  ShowResponse,
 } from '@/core/repositories/base-repository';
 import { User } from '@/domain/users/enterprise/entities/user';
+import { UniqueID } from '../../enterprise/entities/value-objects/unique-id';
 
 export abstract class UserRepository extends BaseRepository<User> {
+  public async findByProperty(
+    params: Partial<User>,
+  ): Promise<ShowResponse<User>> {
+    return { item: {} };
+  }
   public async create(
     params: CreateRequest<Partial<User>>,
   ): Promise<CreateResponse> {
-    return { id: '' };
+    return { id: UniqueID.transform({}) };
   }
 }
--- src/domain/users/application/use-cases/create-user.spec.ts
+++ src/domain/users/application/use-cases/create-user.spec.ts
@@ -1,54 +1,27 @@
-import {
-  CreateRequest,
-  CreateResponse,
-  DeleteRequest,
-  DeleteResponse,
-  IndexRequest,
-  IndexResponse,
-  ShowRequest,
-  ShowResponse,
-  UpdateRequest,
-  UpdateResponse,
-} from '@/core/repositories/base-repository';
-import { User } from '../../enterprise/entities/user';
-import { UserRepository } from '../repositories/user-repository';
+import { InMemoryUserRepository } from 'test/repositories/in-memory-user-repository';
 import { CreateUserUseCase } from './create-user';
 
-const fakeUsersRepository: UserRepository = {
-  create: async (
-    params: CreateRequest<Partial<User>>,
-  ): Promise<CreateResponse> => {
-    return { id: '' };
-  },
-  index: (params: IndexRequest): Promise<IndexResponse<User>> => {
-    throw new Error('Function not implemented.');
-  },
-  show: (params: ShowRequest): Promise<ShowResponse<User>> => {
-    throw new Error('Function not implemented.');
-  },
-  update: (params: UpdateRequest<Partial<User>>): Promise<UpdateResponse> => {
-    throw new Error('Function not implemented.');
-  },
-  delete: (params: DeleteRequest): Promise<DeleteResponse> => {
-    throw new Error('Function not implemented.');
-  },
-};
+let inMemoryUserRepository: InMemoryUserRepository;
+let createUserUseCase: CreateUserUseCase;
 
-test('Create an user', async () => {
-  const createUser = new CreateUserUseCase(fakeUsersRepository);
-
-  const { user: newUser } = await createUser.execute({
-    name: 'User Test',
-    email: 'user@email.test',
-    avatar: 'user.png',
-    password: 'test123',
+describe('Create User', () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUserRepository);
   });
-  console.log(newUser.getSlug());
-  expect(newUser.getId()).not.toBeUndefined();
-  expect(newUser.getId()).not.toBeNull();
-  expect(newUser.getName()).toEqual('User Test');
-  expect(newUser.getSlug()).toEqual('user-test');
-  expect(newUser.getEmail()).toEqual('user@email.test');
-  expect(newUser.getAvatar()).toEqual('user.png');
-  expect(newUser.getPassword()).toEqual('test123');
+  it('should be able to create a user', async () => {
+    const { user: newUser } = await createUserUseCase.execute({
+      name: 'User Test',
+      email: 'user@email.test',
+      avatar: 'user.png',
+      password: 'test123',
+    });
+    expect(newUser.getId()).not.toBeUndefined();
+    expect(newUser.getId()).not.toBeNull();
+    expect(newUser.getName()).toEqual('User Test');
+    expect(newUser.getSlug()).toEqual('user-test');
+    expect(newUser.getEmail()).toEqual('user@email.test');
+    expect(newUser.getAvatar()).toEqual('user.png');
+    expect(newUser.getPassword()).toEqual('test123');
+  });
 });
