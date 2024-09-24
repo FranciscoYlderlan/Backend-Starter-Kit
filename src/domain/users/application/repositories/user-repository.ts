0f84fcc1eaaf8bf3ec38c0311b2099d42638311a
franciscoylderlan <ylderlan@nca.ufma.br>
2024-09-24 10:53:07 -0300

    ✨ feat/BSK-5: Add findByProperty and delete methods to userRepository

--- src/domain/users/application/repositories/user-repository.ts
+++ src/domain/users/application/repositories/user-repository.ts
@@ -2,14 +2,16 @@ import {
   BaseRepository,
   CreateRequest,
   CreateResponse,
+  DeleteRequest,
+  DeleteResponse,
   ShowResponse,
 } from '@/core/repositories/base-repository';
 import { User, UserProps } from '@/domain/users/enterprise/entities/user';
 import { UniqueID } from '../../enterprise/entities/value-objects/unique-id';
 
 export abstract class UserRepository extends BaseRepository<User, UserProps> {
-  public async findByProperty(
-    params: Partial<UserProps>,
+  public async findByProperty<K extends keyof UserProps>(
+    params: Record<K, UserProps[K]>,
   ): Promise<ShowResponse<User>> {
     return { item: {} };
   }
@@ -17,5 +19,9 @@ export abstract class UserRepository extends BaseRepository<User, UserProps> {
     params: CreateRequest<Partial<UserProps>>,
   ): Promise<CreateResponse> {
     return { id: UniqueID.transform({}) };
+  }
+
+  public async delete(params: DeleteRequest): Promise<DeleteResponse> {
+    return { success: true };
   }
 }
