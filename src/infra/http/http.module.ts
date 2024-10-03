6b0c094ddc039b833123cfd81977435f1aad2b38
franciscoylderlan <ylderlan@nca.ufma.br>
2024-10-03 15:30:14 -0300

    ✨ feat/BSK-7: Finalize List Users Controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,12 +1,18 @@
 import { CreateUserUseCase } from '@/domain/users/application/use-cases/create-user';
+import { ListUsersUseCase } from '@/domain/users/application/use-cases/list-users';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { AuthenticateController } from './controllers/authenticate.controller';
 import { CreateUserController } from './controllers/create-user.controller';
+import { ListUsersController } from './controllers/list-users.controller';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateUserController, AuthenticateController],
-  providers: [CreateUserUseCase],
+  controllers: [
+    CreateUserController,
+    AuthenticateController,
+    ListUsersController,
+  ],
+  providers: [CreateUserUseCase, ListUsersUseCase],
 })
 export class HttpModule {}
