ff67b4094b56b1bdc937e911303f78f651772b23
franciscoylderlan <ylderlan@nca.ufma.br>
2024-10-03 10:59:33 -0300

    ✨ feat/BSK-7: Finalize Prisma User Repository

--- src/domain/users/application/use-cases/create-user.ts
+++ src/domain/users/application/use-cases/create-user.ts
@@ -1,5 +1,6 @@
 import { Optional } from '@/core/types/optional';
 import { BaseUseCase } from '@/core/use-cases/base-use-case';
+import { hash } from 'bcrypt';
 import { User, UserProps } from '../../enterprise/entities/user';
 import { UserRepository } from '../repositories/user-repository';
 
@@ -19,6 +20,21 @@ export class CreateUserUseCase extends BaseUseCase<
 
   public async execute(params: CreateUserInput): Promise<CreateUserOutput> {
     const userProps = params;
+
+    const userWithSameEmail = await this.userRepository.findByProperty({
+      email: params.email,
+    });
+
+    if (userWithSameEmail) {
+      throw new Error('User with same email already exists.');
+    }
+
+    const hashedPassword = await hash(userProps.password, 8);
+
+    Object.assign(userProps, {
+      ...userProps,
+      password: hashedPassword,
+    });
 
     const user = User.create(userProps);
 
