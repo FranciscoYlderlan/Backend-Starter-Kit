17bd3ad83772bd34bcbbf97fddc8b8c9407e881b
franciscoylderlan <ylderlan@nca.ufma.br>
2024-10-03 11:45:42 -0300

    ✨ feat/BSK-7: Finalize Create User Controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,3 +1,4 @@
+import { CreateUserUseCase } from '@/domain/users/application/use-cases/create-user';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { AuthenticateController } from './controllers/authenticate.controller';
@@ -6,5 +7,6 @@ import { CreateUserController } from './controllers/create-user.controller';
 @Module({
   imports: [DatabaseModule],
   controllers: [CreateUserController, AuthenticateController],
+  providers: [CreateUserUseCase],
 })
 export class HttpModule {}
