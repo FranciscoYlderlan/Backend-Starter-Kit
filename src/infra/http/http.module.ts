254c73359d6a38798fd45196063ad051eb5a9f7d
franciscoylderlan <ylderlan@nca.ufma.br>
2024-10-08 16:05:41 -0300

    ✨ feat/BSK-7: Implement encryption classes

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,7 +1,10 @@
+import { AuthenticateUserUseCase } from '@/domain/users/application/use-cases/authenticate-user';
 import { CreateUserUseCase } from '@/domain/users/application/use-cases/create-user';
 import { DeleteUserUseCase } from '@/domain/users/application/use-cases/delete-user';
+import { FindUserUseCase } from '@/domain/users/application/use-cases/find-user';
 import { ListUsersUseCase } from '@/domain/users/application/use-cases/list-users';
 import { Module } from '@nestjs/common';
+import { CryptographyModule } from '../cryptography/cryptography.module';
 import { DatabaseModule } from '../database/database.module';
 import { AuthenticateController } from './controllers/authenticate.controller';
 import { CreateUserController } from './controllers/create-user.controller';
@@ -9,13 +12,19 @@ import { DeleteUserController } from './controllers/delete-user.controller';
 import { ListUsersController } from './controllers/list-users.controller';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateUserController,
     AuthenticateController,
     ListUsersController,
     DeleteUserController,
   ],
-  providers: [CreateUserUseCase, ListUsersUseCase, DeleteUserUseCase],
+  providers: [
+    AuthenticateUserUseCase,
+    ListUsersUseCase,
+    FindUserUseCase,
+    CreateUserUseCase,
+    DeleteUserUseCase,
+  ],
 })
 export class HttpModule {}
